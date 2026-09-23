cb8b0c5d2adf293cae23da7978aa51620532d2cf
Benjamin Kovalick <Benjamin.Kovalick@dimensional.com>
2026-09-23 10:47:29 -0400

    Code cleanup

--- src/portfolio_optimizers/optimizers/multi_period_optimizer.py
+++ src/portfolio_optimizers/optimizers/multi_period_optimizer.py
@@ -1,101 +1,9 @@
 import gurobipy as gp
+from gurobipy import GRB
 import pandas as pd
 import numpy as np
-from pathlib import Path
-from gurobipy import GRB
+from portfolio_optimizers.optimizers.configuration import PortfolioConfiguration
 
-from portfolio_optimizers.signals.market_signals import SyntheticSignals, GARCHSignals
-
-class PortfolioConfiguration:
-    def __init__(self, config: dict):
-        self._config = config
-
-    @property
-    def securities(self) -> list:
-        self._securities = self.market_data.columns
-        if self._securities is None:
-            raise ValueError("Securities are missing, likely due to missing market data.")
-        return self._securities
-        
-    @property
-    def n_constituents(self) -> int:
-        self._n_constituents = len(self.securities) 
-        if self._n_constituents is None:
-            raise ValueError("Constituents are missing, likely due to missing market data.")
-
-        return self._n_constituents  
-
-    @property
-    def market_data(self) -> pd.DataFrame: 
-        self._market_data = self._config.get("market_data", None)
-        if self._market_data is None:
-            raise ValueError("No market data provided, therefore optimization is stopping")
-
-        return self._market_data
-
-    @property
-    def market_returns(self) -> pd.DataFrame:
-        self._market_returns = self.market_data.pct_change().iloc[1:] * 100
-        if self._market_returns is None:
-            raise ValueError("Market Returns are missing. Please provide proper market data")
-        return self._market_returns
-    
-    @property
-    def mu_levels(self) -> np.ndarray:
-        self._mu_levels = self._config.get("mu_levels", None)
-        if self._mu_levels is None:
-            raise ValueError("Mean Returns are missing. Please provide proper market data")
-
-        return self._mu_levels
-
-    @property
-    def sigma_levels(self) -> np.ndarray:
-        self._sigma_levels = self._config.get("sigma_levels", None)
-        if self._sigma_levels is None:
-            raise ValueError("Sigma levels are missing. Please provide proper market data")                 
-
-        return self._sigma_levels
-
-    @property
-    def current_weights(self) -> np.ndarray:
-        current_weights = self._config.get("current_weights", None)
-        if current_weights is None or len(current_weights) == 0:
-            self._current_weights = np.repeat(1.0 / self.n_constituents, self.n_constituents)
-        else:
-            if len(current_weights) != self.n_constituents:
-                raise ValueError("Current weights must match the number of market data columns")
-            self._current_weights = np.asarray(current_weights, dtype=float)    
-
-        return self._current_weights
-
-    @property
-    def apply_shrinkage(self) -> bool:
-        return self._config.get("apply_shrinkage", False)
-
-    @property
-    def risk_aversion(self) -> float:
-        return self._config.get("risk_aversion", 0.0)
-
-    @property
-    def variance_level(self) -> float:
-        return self._config.get("variance_level", 3.5)
-
-    @property
-    def time_horizon(self) -> int:
-        return self._config.get("time_horizon", 3)
-
-    @property
-    def buy_cost(self) -> float:
-        return self._config.get("buy_cost", 0.002)
-
-    @property
-    def sell_cost(self) -> float:
-        return self._config.get("sell_cost", 0.001)
-
-    @property
-    def hold_cost(self) -> float:
-        return self._config.get("hold_cost", 0.0005)
-    
 class MultiPeriodOptimizer:
     def __init__(self, env: gp.Env):
         """
@@ -167,13 +75,48 @@ class MultiPeriodOptimizer:
             return f"STATUS_{self.model.status}"
         
     def _create_decision_variables(self): 
-        """Create the decision variables"""
-        self.trades = self.model.addVars(range(1, self.time_horizon+1), range(self.n_constituents), vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, name="trades")
-        self.buy_trades = self.model.addVars(range(1, self.time_horizon+1), range(self.n_constituents), vtype=GRB.CONTINUOUS, lb=0.0, name="buy_trades")
-        self.sell_trades = self.model.addVars(range(1, self.time_horizon+1), range(self.n_constituents), vtype=GRB.CONTINUOUS, lb=0.0, name="sell_trades")
-        self.is_buying = self.model.addVars(range(1, self.time_horizon+1), range(self.n_constituents), vtype=gp.GRB.BINARY, name="is_buying")
-        self.is_selling = self.model.addVars(range(1, self.time_horizon+1), range(self.n_constituents), vtype=gp.GRB.BINARY, name="is_selling")        
-        self.optimal_weights = self.model.addMVar((self.time_horizon+1, self.n_constituents), vtype=GRB.CONTINUOUS, lb=0.0, name="optimal_weights")
+        """Create decision variables"""
+        self.trades = self.model.addVars(
+            range(1, self.time_horizon+1), 
+            range(self.n_constituents), 
+            vtype=GRB.CONTINUOUS, 
+            lb=-GRB.INFINITY, 
+            name="trades"
+        )
+
+        self.buy_trades = self.model.addVars(
+            range(1, self.time_horizon+1), 
+            range(self.n_constituents), 
+            vtype=GRB.CONTINUOUS, 
+            lb=0.0, 
+            name="buy_trades"
+        )
+
+        self.sell_trades = self.model.addVars(
+            range(1, self.time_horizon+1), 
+            range(self.n_constituents), 
+            vtype=GRB.CONTINUOUS, lb=0.0, 
+            name="sell_trades"
+        )
+
+        self.is_buying = self.model.addVars(
+            range(1, self.time_horizon+1), 
+            range(self.n_constituents), 
+            vtype=gp.GRB.BINARY, 
+            name="is_buying")
+        
+        self.is_selling = self.model.addVars(
+            range(1, self.time_horizon+1), 
+            range(self.n_constituents), 
+            vtype=gp.GRB.BINARY, 
+            name="is_selling")
+        
+        self.optimal_weights = self.model.addMVar(
+            (self.time_horizon+1, self.n_constituents), 
+            vtype=GRB.CONTINUOUS, 
+            lb=0.0, 
+            name="optimal_weights"
+        )
 
     def _setup_constraints(self):
         """Set up the constraints for the optimization model"""
